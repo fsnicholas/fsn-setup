709702689e3d60e5b9bc9c3131fba1fa69659b2a
fsnicholas <frank.nicholas@gmail.com>
2023-12-18 17:40:15 +1030

    spelling correction  setup-git

--- setup-git-v5.sh
+++ setup-git-v5.sh
@@ -27,14 +27,14 @@ echo
 
 # Read before using it.
 # https://www.atlassian.com/git/tutorials/undoing-changes/git-reset
-# git reset --hard orgin/master
+# git reset --hard origin/master
 # ONLY if you are very sure and no coworkers are on your github.
 
 # Command that have helped in the past
 # Force git to overwrite local files on pull - no merge
 # git fetch all
 # git push --set-upstream origin master
-# git reset --hard orgin/master
+# git reset --hard origin/master
 
 
 #setting up git
