1ceac3bf0fbfb6c9ad1365a2dccfbf913ba4014a
fsnicholas <frank.nicholas@gmail.com>
2023-12-20 16:39:28 +1030

    corrected spelling

--- Personal/fsn-034-copy-doas.sh
+++ Personal/fsn-034-copy-doas.sh
@@ -2,7 +2,7 @@
 
 # setting up new version of doas.conf
 
-#Colour Varibles
+#Colour Variables
 RED='\033[0;31m'
 BRED='\033[1;31m'         # Bold Red
 BYell='\033[1;33m'         # Bold Yellow
@@ -15,4 +15,3 @@ echo
 # delete if file exists
 sudo rm -f -- /etc/doas.conf
 sudo cp $installed_dir/settings/doas/doas.conf   /etc/doas.conf
-
