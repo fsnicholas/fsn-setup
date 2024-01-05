20b56734f91c9d3d16de3b75dd5061fcf43beda1
fsnicholas <frank.nicholas@gmail.com>
2024-01-05 15:46:01 +1030

    amended variety install folder & script

--- Personal/fsn-040-setup-variety.sh
+++ Personal/fsn-040-setup-variety.sh
@@ -12,6 +12,10 @@
 #tput setaf 6 = cyan
 #tput setaf 7 = gray
 #tput setaf 8 = light blue
+RED='\033[0;31m'
+BRED='\033[1;31m'         # Bold Red
+BYell='\033[1;33m'         # Bold Yellow
+NC='\033[0m' # No Color
 ##################################################################################################################
 
 installed_dir=$(dirname $(readlink -f $(basename `pwd`)))
@@ -26,11 +30,12 @@ echo "##################################################################"
 tput sgr0
 
 # for variety
+echo -e  "${BYell}Installing needed software for variety${NC}"
 sudo pacman -S --noconfirm --needed python-setuptools
 sudo pacman -S --noconfirm --needed python-cairo
 
 echo
-echo "Installing personal settings of variety"
+echo -e  "${BYell}Installing personal settings of variety${NC}"
 [ -d $HOME"/.config/variety" ] || mkdir -p $HOME"/.config/variety"
 cp -vf $installed_dir/settings/variety/variety.conf ~/.config/variety/
 echo
